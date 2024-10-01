325c634ec6085659012e52fb9f21a317182b20da
Zquan315 <22521190@gm.uit.edu.vn>
2024-10-01 10:52:31 +0700

    Thêm giao diện Quên mật khẩu, Đổi mật khẩu

--- IEM/app/src/main/java/ex/g1/iem/changePass_fromForgot_UI.java
+++ IEM/app/src/main/java/ex/g1/iem/changePass_fromForgot_UI.java
@@ -1,8 +1,6 @@
 package ex.g1.iem;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,21 +8,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class changePass_fromForgot_UI extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-         //Xử lý sự kiện khi nút "Quên mật khẩu" được nhấn
-        Button forgotPass = findViewById(R.id.forgotPasswordTextBtn);
-        forgotPass.setOnClickListener(v -> {
-            startActivity(new Intent(MainActivity.this, forgotPass_UI.class));
-        });
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_change_pass_from_forgot_ui);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.changePass_fromForgot), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
--- IEM/app/src/main/java/ex/g1/iem/forgotPass_UI.java
+++ IEM/app/src/main/java/ex/g1/iem/forgotPass_UI.java
@@ -10,21 +10,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class forgotPass_UI extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_forgot_pass_ui);
 
-         //Xử lý sự kiện khi nút "Quên mật khẩu" được nhấn
-        Button forgotPass = findViewById(R.id.forgotPasswordTextBtn);
-        forgotPass.setOnClickListener(v -> {
-            startActivity(new Intent(MainActivity.this, forgotPass_UI.class));
+        //Xử lý sự kiện khi nút "Xác nhận" được nhấn
+        Button confirmBtn = findViewById(R.id.confirmBtn);
+        confirmBtn.setOnClickListener(v -> {
+            startActivity(new Intent(forgotPass_UI.this, changePass_fromForgot_UI.class));
         });
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.forgotPass), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
